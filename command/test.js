c7c05a73d6e6a6e7839631dc84bb17d101433e4d
huakang <741180788@qq.com>
2022-11-03 18:49:35 +0800

    update

--- command/test.js
+++ command/test.js
@@ -1,40 +1,32 @@
-const path = require('path')
+// const path = require('path')
 const inquirer = require('inquirer')
-const clear = require('clear');
+// const clear = require('clear');
 
-const { blue, loading } = require('../lib/utils')
-const inquirer = require('inquirer');
+// const { blue, loading } = require('../lib/utils')
 
 const questions = [
   {
-
+    name: 'conf',
+    type: 'confirm',
+    message: '是否创建新的项目？'
+  },
+  {
+    name: 'name',
+    message: '请输入项目名称？',
+    when: res => Boolean(res.conf)
+  },
+  {
+    type: 'checkbox',
+    message: '请选择公共管理状态？',
+    name: 'state',
+    choices: ['mobx', 'redux'],
+    when: res => Boolean(res.conf)
   }
 ]
 
 function test(name, options) {
-  blue(JSON.stringify(name))
-  console.log('')
-  blue(JSON.stringify())
   inquirer
-    .prompt([
-      {
-        name: 'conf',
-        type: 'confirm',
-        message: '是否创建新的项目？'
-      },
-      {
-        name: 'name',
-        message: '请输入项目名称？',
-        when: res => Boolean(res.conf)
-      },
-      {
-        type: 'list',
-        message: '请选择公共管理状态？',
-        name: 'state',
-        choices: ['mobx', 'redux'],
-        when: res => Boolean(res.conf)
-      }
-    ])
+    .prompt(questions)
     .then(answers => {
       console.log(answers)
     })
